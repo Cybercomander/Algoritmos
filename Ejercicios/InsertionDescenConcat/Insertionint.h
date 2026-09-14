0f40ac7d00df2f422fdcd041bd30512bb9925111
Carlos Ariel Ureta Armenta <117868321+Cybercomander@users.noreply.github.com>
2026-09-14 13:10:47 -0700

    feat : Ejercicio InsertionDescenConcat

---
+++ Ejercicios/InsertionDescenConcat/Insertionint.h
@@ -0,0 +1,11 @@
+#ifndef INSERTIONINT_H
+#define INSERTIONINT_H
+
+    #include <stdio.h>
+    #include <math.h>
+    #include <stdlib.h>
+
+    void insertion(int arr[], int n );
+    int concat(int arr[], int n );
+
+#endif
